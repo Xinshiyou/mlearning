3496b7296df7eb5c3df753e064444971cafc748c
shiyou.xin <shiyou.xin@owitho.com>
2018-02-28 14:35:32 +0800

    modify weka-project package name, add java version to java1.8

---
+++ mlearning-weka/src/main/java/com/hundun/mlearning/weka/App.java
@@ -0,0 +1,16 @@
+package com.hundun.mlearning.weka;
+
+/**
+ * @DESC Hello world!
+ */
+public class App {
+	
+	/**
+	 * @DESC for weka tests
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
