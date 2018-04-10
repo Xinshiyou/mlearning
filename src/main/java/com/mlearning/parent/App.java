bc63fa5f743b53cc2f8fb3a2024004837cdb24b8
shiyou.xin <shiyou.xin@owitho.com>
2018-04-10 17:17:39 +0800

    add modify

--- src/main/java/com/mlearning/parent/App.java
+++ src/main/java/com/mlearning/parent/App.java
@@ -4,10 +4,8 @@ package com.mlearning.parent;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
